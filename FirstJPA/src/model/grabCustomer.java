8b7d12800b19b80a78ae8467cc03822f7ac992f4
Ya-2015 <ya.liu@infosys.com>
2015-11-02 16:50:38 -0500

    JPA

---
+++ FirstJPA/src/model/grabCustomer.java
@@ -0,0 +1,21 @@
+package model;
+
+import javax.persistence.EntityManager;
+
+public class grabCustomer {
+	public static void main(String[] args) {
+		EntityManager em = DBUtil.getEmFactory().createEntityManager();
+		try {
+			model.DemoCustomer cust = em.find(model.DemoCustomer.class, (long)2);
+			System.out.println(cust.getCustFirstName());
+		} catch (Exception e){
+			System.out.println(e);
+		} finally {
+			em.close();
+			System.out.println("cerrado!");
+		}
+
+	}
+
+
+}
